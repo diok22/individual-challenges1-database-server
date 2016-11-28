85bb7091837a1e1c83d87cc3745f32ad10d35808
Dionysis <Dionysis.k22@gmail.com>
2016-11-28 12:47:17 +0000

    enables sessions

--- app.rb
+++ app.rb
@@ -1,24 +1,27 @@
 require 'sinatra/base'
+require_relative './lib/models/key_in_memory'
+require_relative './lib/models/value_in_memory'
+
 
 class DatabaseServer < Sinatra::Base
+
+  enable :sessions
 
   get '/' do
     'Hello DatabaseServer!'
   end
 
   get '/set' do
-    @memory_array = []
-    key_symbol = params.keys[0].to_sym
-    value_symbol = params.values[0].to_sym
+    p params.keys[0].to_sym
+    key = KeyInMemory.new(params.keys[0].to_sym)
+    value = ValueInMemory.new(params.values[0].to_sym)
     hash = {}
-    hash.store(key_symbol, value_symbol)
-    p hash
-    @memory_array << hash
-    p @memory_array
+    hash.store(key, value)
+    array = Array.new
+    session.push(array)
+    p session
+    redirect '/'
   end
-
-  
-
 
 
   # start the server if ruby file executed directly
---
+++ lib/models/key_in_memory.rb
@@ -0,0 +1,7 @@
+class KeyInMemory
+  attr_reader :key
+
+  def initialize(key)
+    @key = key
+  end
+end
